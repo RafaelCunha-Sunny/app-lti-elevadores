29a62c5b2dbce4b12aabd37f3ac735ea4b4e4225
Rafael Cunha <r.a.f.a.c.u.n.h.a.a@gmail.com>
2026-03-12 19:13:56 -0300

    feat: adicionando novas funcionalidades como, mascaras de cnpj, cep e calculadora financeira

--- motor.js
+++ motor.js
@@ -36,7 +36,7 @@ inputCnpjCpf.addEventListener('input', async function (event) {
             if (!dados.message) {
                 inputNomeCliente.value = dados.razao_social;
 
-                let cepEmpresa = dados.cep.replace(/^(d{5})(\d)/, "$1-2");
+                let cepEmpresa = dados.cep.replace(/^(\d{5})(\d)/, "$1-$2");
                 document.getElementById('cep').value = cepEmpresa;
 
                 document.getElementById('endereco').value = `${dados.logradouro}, ${dados.bairro}, ${dados.municipio}/${dados.uf}`;
@@ -57,12 +57,16 @@ inputCnpjCpf.addEventListener('input', async function (event) {
 // 2. MÁSCARA CEP E BUSCA DE ENDEREÇO
 // =======================================================
 const inputCep = document.getElementById('cep');
-const inputEndereco = document.getElementById('endereco')
+const inputEndereco = document.getElementById('endereco');
 
 inputCep.addEventListener('input', async function (event) {
     let cep = event.target.value;
 
     cep = cep.replace(/\D/g, "");
+
+     if (cep.length > 8) {
+        cep = cep.substring(0, 8);
+    }
 
     let cepParaBusca = cep;
 
@@ -80,7 +84,68 @@ inputCep.addEventListener('input', async function (event) {
             }
             }
             catch (erro) {
-                console.log("Erro na conexão com a API")
+                console.log("Erro na conexão com a API");
             }
     }
 })
+
+// =======================================================
+// GRATUIDADE (ESCONDER/MOSTRAR CAIXOTE)
+// =======================================================
+
+const radioSim = document.getElementById('gratuidadeSim');
+const radioNao = document.getElementById('gratuidadeNao');
+const areaGratuidade = document.getElementById('area-gratuidade');
+
+function alternarCaixote() {
+    if (radioSim.checked) {
+        areaGratuidade.style.display = 'block';
+        atualizarMesesGratuidade();
+    } else {
+        areaGratuidade.style.display = 'none';
+        atualizarMesesGratuidade();
+    }
+}
+
+radioSim.addEventListener('change', alternarCaixote);
+radioNao.addEventListener('change', alternarCaixote);
+
+// =======================================================
+// 4. CALCULADORA FINANCEIRA (VALOR UNITÁRIO X QUANTIDADE)
+// =======================================================
+
+const inputQuantidade = document.getElementById('quantidade');
+const inputValorEquipamento = document.getElementById('valorEquipamento');
+const inputValorTotal = document.getElementById('valorTotal');
+
+function calcularTotal() {
+    let qtd = parseInt(inputQuantidade.value) || 0;
+    let valorSujo = inputValorEquipamento.value.replace(/\D/g,"");
+    let valorNumerico = (parseFloat(valorSujo) / 100) || 0;
+
+    let total = qtd * valorNumerico;
+
+    if (total > 0) {
+        inputValorTotal.value = total.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL' });
+        } else {
+            inputValorTotal.value = "";
+        }
+    }
+
+    inputValorEquipamento.addEventListener('input', function(event){
+        let valor = event.target.value.replace(/\D/g, "");
+
+        if (valor === "") {
+            event.target.value = "";
+            calcularTotal();
+            return;
+        }
+
+        valor = (parseFloat(valor) / 100 ).toLocaleString('pt-BR', {style: 'currency', currency: 'BRL' });
+            event.target.value = valor;
+            calcularTotal();
+    })
+
+    if (inputQuantidade) {
+        inputQuantidade.addEventListener('change', calcularTotal);
+    }
